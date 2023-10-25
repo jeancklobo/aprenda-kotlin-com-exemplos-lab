50ed5ae76be33f3754d7a09ab7066e01f1d381ec
jeancklobo <97307434+jeancklobo@users.noreply.github.com>
2023-10-25 11:38:18 -0300

    resolução

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,72 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String,val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    override fun toString(): String{
+        var texto: String
+        
+        texto = "Formação: $nome\nNível: "
+        
+        when(nivel){
+            Nivel.BASICO -> texto += "Básico"
+            Nivel.INTERMEDIARIO -> texto += "Intermediário"
+            Nivel.AVANCADO -> texto += "Avançado"
+            
+        }
+        
+        texto += "\nLista de Conteudo:\n"
+        
+        for(c in conteudos){
+            texto += "${c.nome}\n"
+        }
+        
+        texto += "\nAlunos:\n${listarAlunos()}"
+        
+        return texto
+    }
+    
+    fun matricular(vararg usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        for(u: Usuario in usuario){
+            inscritos.add(u)
+        }
+    }
+    
+    fun listarAlunos(): String{
+        var texto:String = ""
+        for(i in inscritos){
+            texto += "${i.nome}\n"
+        }
+        return texto
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("Pedro")
+    
+    val kotlin = ConteudoEducacional(nome = "Desmistificando Kotlin para programadores Java",
+                                     duracao = 6, 
+                                     nivel = Nivel.BASICO)
+    
+    val java = ConteudoEducacional("Ganhando produtividade com Java e Spring Boot ", 
+                                   10, 
+                                   Nivel.AVANCADO)
+    
+    val formacao = Formacao("Backend com Kotlin e Java", Nivel.INTERMEDIARIO,listOf(kotlin,java))
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2, usuario3)
+    
+    println(formacao)
+    
 }
